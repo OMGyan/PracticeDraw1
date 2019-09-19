01cffe1f6aedae1f4c495c632c8c417d0dcc7a47
yanxun <1768695841@qq.com>
2019-09-19 19:37:52 +0800

    优化线起点算法

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice11PieChartView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice11PieChartView.java
@@ -17,6 +17,8 @@ public class Practice11PieChartView extends View {
     private RectF normalRectf;
     private Paint linePaint;
 
+    private int centerX,centerY,radius;
+
     public Practice11PieChartView(Context context) {
         this(context,null);
     }
@@ -28,16 +30,20 @@ public class Practice11PieChartView extends View {
     public Practice11PieChartView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
 
-        mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        normalRectf = new RectF(150, 300, 450, 600);
+         mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+         normalRectf = new RectF(150, 300, 450, 600);
 
-        textPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        textPaint.setColor(Color.WHITE);
-        textPaint.setTextSize(28);
+         textPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+         textPaint.setColor(Color.WHITE);
+         textPaint.setTextSize(28);
 
-        linePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        linePaint.setStrokeWidth(2);
-        linePaint.setColor(Color.WHITE);
+         linePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+         linePaint.setStrokeWidth(2);
+         linePaint.setColor(Color.WHITE);
+
+         centerX = (int) ((normalRectf.right - normalRectf.left) / 2 + normalRectf.left);
+         centerY = (int) ((normalRectf.bottom - normalRectf.top) / 2 + normalRectf.top);
+         radius = (int) ((normalRectf.right - normalRectf.left) / 2);
     }
 
     @Override
@@ -49,7 +55,16 @@ public class Practice11PieChartView extends View {
 //        练习内容：使用各种 Canvas.drawXXX() 方法画饼图
           mPaint.setColor(Color.BLUE);
           canvas.drawArc(normalRectf,80,90,true,mPaint);
-          float[] points = {210,570,190,595,190,595,100,595};
+         /* 圆点坐标：(x0,y0)
+           半径：r
+           角度：a0
+
+          则圆上任一点为：（x1,y1）
+          x1 = x0 + r * cos(ao * 3.14 /180 )
+          y1 = y0 + r * sin(ao * 3.14 /180 )*/
+          float x =((float) (centerX + radius * Math.cos(125 * Math.PI / 180)));
+          float y =((float) (centerY + radius * Math.sin(125 * Math.PI / 180)));
+          float[] points = {x,y,190,595,190,595,100,595};
           canvas.drawLines(points,linePaint);
           canvas.drawText("KitKat",10,600,textPaint);
 
@@ -57,40 +72,59 @@ public class Practice11PieChartView extends View {
 
           mPaint.setColor(Color.GREEN);
           canvas.drawArc(normalRectf,15,63,true,mPaint);
-          float[] points1 = {405,555,435,585,435,585,500,585};
+          float x1 = (float)(centerX + radius * Math.cos(46.5 * Math.PI / 180));
+          float y1 = (float)(centerY + radius * Math.sin(46.5 * Math.PI / 180));
+          float[] points1 = {x1,y1,435,585,435,585,500,585};
           canvas.drawLines(points1,linePaint);
           canvas.drawText("Jelly Bean",520,590,textPaint);
 
+
           mPaint.setColor(Color.RED);
           canvas.drawArc(normalRectf,5,8,true,mPaint);
-          float[] points2 = {449,472,484,472,484,472,510,502,510,502,535,502};
+          float x2 = (float)(centerX + radius * Math.cos(9 * Math.PI / 180));
+          float y2 = (float)(centerY + radius * Math.sin(9 * Math.PI / 180));
+          float[] points2 = {x2,y2,484,472,484,472,510,502,510,502,535,502};
           canvas.drawLines(points2,linePaint);
           canvas.drawText("Ice Cream Sandwich",555,507,textPaint);
 
 
           mPaint.setColor(Color.YELLOW);
           canvas.drawArc(normalRectf,-2,5,true,mPaint);
-          float[] points3 = {450,452,475,452,475,452,495,423,495,423,520,423};
+          float x3 = (float)(centerX + radius * Math.cos(0.5 * Math.PI / 180));
+          float y3 = (float)(centerY + radius * Math.sin(0.5 * Math.PI / 180));
+          float[] points3 = {x3,y3,475,452,475,452,495,423,495,423,520,423};
           canvas.drawLines(points3,linePaint);
           canvas.drawText("Gingerbread",540,428,textPaint);
 
           mPaint.setColor(Color.BLACK);
           canvas.drawArc(normalRectf,-4,-65,true,mPaint);
-          float[] points4 = {421,360,460,332,460,332,530,332};
+          float x4 = (float)(centerX + radius * Math.cos(-36.5 * Math.PI / 180));
+          float y4 = (float)(centerY + radius * Math.sin(-36.5 * Math.PI / 180));
+          float[] points4 = {x4,y4,460,332,460,332,530,332};
           canvas.drawLines(points4,linePaint);
           canvas.drawText("Marshmallow",550,337,textPaint);
 
           mPaint.setColor(Color.LTGRAY);
           canvas.drawArc(new RectF(140, 290, 440, 590),-71,-117,true,mPaint);
-          float[] points5 = {190,328,170,303,170,303,130,303};
+
+           centerX = (int) ((440 - 140) / 2 + 140);
+           centerY = (int) ((590 - 290) / 2 + 290);
+           radius = (int) ((440 - 140) / 2);
+
+
+          float x5 = (float)(centerX + radius * Math.cos(-129.5 * Math.PI / 180));
+          float y5 = (float)(centerY + radius * Math.sin(-129.5 * Math.PI / 180));
+          float[] points5 = {x5,y5,170,303,170,303,130,303};
           canvas.drawLines(points5,linePaint);
           canvas.drawText("Lollipop",12,310,textPaint);
 
 
-        //画标题
-        Paint paint3 = new Paint(Paint.ANTI_ALIAS_FLAG);
-        paint3.setColor(Color.WHITE);
-        paint3.setTextSize(50);
-        canvas.drawText("饼图",330,690,paint3);
+          //画标题
+          Paint paint3 = new Paint(Paint.ANTI_ALIAS_FLAG);
+          paint3.setColor(Color.WHITE);
+          paint3.setTextSize(50);
+          canvas.drawText("饼图",330,690,paint3);
     }
+
+
 }
